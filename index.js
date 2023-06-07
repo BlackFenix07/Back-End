634751be1fc144e76da42341a1712fbf385eefb5
Andrés Zapata <andresmauricio030400@gmail.com>
2023-06-07 11:08:49 -0500

    Clase de Routing con Express.js

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Primer server en Express');
 });
 
+app.get('/new-route', (req, res) => {
+  res.send('Soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000,
+  });
+});
+
 app.listen(port, () => {
   console.log('Mi puerto: ' + port);
 });
