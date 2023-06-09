0f64cdaf8dc8822c70a255b3b09c8484a8a96e0e
Andrés Zapata <andresmauricio030400@gmail.com>
2023-06-09 10:34:03 -0500

    Clase de Validación de datos con Joi

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,11 +1,9 @@
 function logErrors(err, req, res, next) {
-  console.log('logErrors');
   console.error(err);
   next(err);
 }
 
 function errorHandler(err, req, res, next) {
-  console.log('errorHandler');
   res.status(500).json({
     message: err.message,
     stack: err.stack,
@@ -16,9 +14,8 @@ function boomErrorHandler(err, req, res, next) {
   if (err.isBoom) {
     const { output } = err;
     res.status(output.statusCode).json(output.payload);
-  } else {
-    next(err);
   }
+  next(err);
 }
 
 module.exports = { logErrors, errorHandler, boomErrorHandler };
