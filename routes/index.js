48159c900539b29b63f419663e4744612ac9be41
Andrés Zapata <andresmauricio030400@gmail.com>
2023-07-10 16:22:05 -0500

    Clase de Relaciones uno a uno

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const productsRouter = require('./productsRouter');
 const categoriesRouter = require('./categoriesRouter');
 const usersRouter = require('./usersRouter');
+const orderRouter = require('./ordersRouter');
+const customersRouter = require('./customersRouter');
 
 function routerApi(app) {
   const router = express.Router();
@@ -10,6 +12,8 @@ function routerApi(app) {
   router.use('/products', productsRouter);
   router.use('/categories', categoriesRouter);
   router.use('/users', usersRouter);
+  router.use('/orders', orderRouter);
+  router.use('/customers', customersRouter);
 }
 
 module.exports = routerApi;
