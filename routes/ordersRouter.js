e53c36f3a394b8d21e8ebaefadcf9b1048099ee7
Andrés Zapata <andresmauricio030400@gmail.com>
2023-07-12 13:57:19 -0500

    Clase de Resolviendo las relaciones muchos a muchos

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const OrderService = require('../services/orderService');
 const validatorHandler = require('../middlewares/validatorHandler');
-const { getOrderSchema, createOrderSchema } = require('../schemas/orderSchema');
+const { getOrderSchema, createOrderSchema, addItemSchema } = require('../schemas/orderSchema');
 
 const router = express.Router();
 const service = new OrderService();
@@ -29,6 +29,20 @@ router.post(
       const body = req.body;
       const newOrder = await service.create(body);
       res.status(201).json(newOrder);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post(
+  '/add-item',
+  validatorHandler(addItemSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newItem = await service.addItem(body);
+      res.status(201).json(newItem);
     } catch (error) {
       next(error);
     }
